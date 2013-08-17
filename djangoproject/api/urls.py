3dd2a771b71ac4744de6ac8e72b6a4ed586036aa
Rod Xavier Bondoc <rod_xavier_14@yahoo.com>
2013-08-17 16:21:51 +0800

    requirements.txt

--- djangoproject/api/urls.py
+++ djangoproject/api/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, url
 
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken.views import obtain_auth_token
 
 from api import views
 
 urlpatterns = patterns('snippets.views',
     url(r'^accounts/register/$', views.UserRegistrationAPIView.as_view(), name='api_accounts_register'),
-    url(r'^accounts/login/$', 'rest_framework.authtoken.views.obtain_auth_token', name='api_accounts_login'),
+    url(r'^accounts/login/$', obtain_auth_token, name='api_accounts_login'),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
