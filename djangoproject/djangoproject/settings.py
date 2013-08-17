c3a57860ae9db5d64fd14dcc105ea4177a80b8e1
Rod Xavier Bondoc <rod_xavier_14@yahoo.com>
2013-08-18 01:25:23 +0800

    Set STATIC_URL

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -63,7 +63,7 @@ MEDIA_URL = 'http://team-josh-devcup-2013.s3.amazonaws.com/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static-prod')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
